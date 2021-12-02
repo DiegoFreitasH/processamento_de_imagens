3b1a6312572c79650bb82a12135bdc3e0a398985
Diego Holanda <diegofreitasholanda@gmail.com>
2021-12-02 19:06:58 -0300

    Add curve editor class

--- curve.py
+++ curve.py
@@ -1,45 +1,112 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
-import tkinter as tk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from matplotlib.figure import Figure
+import tkinter as tk
 
-points = np.array([[0, 0], [.3, .9], [1, 1]])
+class CurveEditor:
 
-data = np.linspace(0, 1, 50)
+    def __init__(self, app=None) -> None:
+        self.root = tk.Toplevel()
+        self.app = app
 
-def get_interpolation_polynomial(points: np.ndarray):
+        self.points_x = [0, 1]
+        self.points_y = [0, 1]
 
-    def poly(x):
-        n = len(points)
-        y = 0
-        for i in range(n):
-            prod = 1
-            for j in range(n):
-                if i == j: continue
-                prod *= points[i, 1] * (x - points[j, 0])/(points[i, 0] - points[j, 0])
-            y += prod
-        return y
+        tk.Label(self.root, text='X:').grid(column=0, row=1)
+        self.input_x = tk.Entry(self.root, width=5)
+        self.input_x.grid(column=1, row=1)
+        
+        tk.Label(self.root, text='Y:').grid(column=2, row=1)
+        self.input_y = tk.Entry(self.root, width=5)
+        self.input_y.grid(column=3, row=1)
+
+        self.draw_curve = tk.Button(
+            self.root,
+            text="Add Point",
+            command=self.add_point
+        )
+        self.draw_curve.grid(column=1, row=2, columnspan=2)
+        apply_controls = tk.Button(
+            self.root,
+            text="Apply",
+            command=self.apply
+        )
+        apply_controls.grid(column=3, row=2, columnspan=2)
+        reset_controls = tk.Button(
+            self.root,
+            text="Reset",
+            command=self.reset
+        )
+        reset_controls.grid(column=5, row=2, columnspan=2)
+
+        self.poly = self.get_polynomial_interpolation()
+        figure = plt.Figure(figsize=(5,4), dpi=100)
+        ax = figure.add_subplot(111)
+        self.chart = FigureCanvasTkAgg(figure, self.root)
+        self.line, = ax.plot(self.points_x, self.points_y)
+        self.chart.get_tk_widget().grid(column=0, row=10, columnspan=10,padx=20, pady=20)
+        self.root.mainloop()
+
+    def add_point(self):
+        try:
+            x = float(self.input_x.get())
+            y = float(self.input_y.get())
+            x = max(min(x, 1), 0)
+            y = max(min(y, 1), 0)
+            if x in self.points_x:
+                i = self.points_x.index(x)
+                self.points_y[i] = y
+            else:
+                self.points_x.append(x)
+                self.points_y.append(y)
+            self.poly = self.get_polynomial_interpolation()
+            self.update_curve()
+        except Exception as e:
+            print(e)
+
+    def remove_last_points(self):
+        if len(self.points_x > 2):
+            self.points_x.pop()
+            self.points_y.pop()
+        
+    def update_curve(self):
+        x = np.linspace(0,1,30)
+        y = self.poly(x)
+        
+        self.line.set_xdata(x)
+        self.line.set_ydata(y)
+        self.chart.draw()
     
-    return poly
+    def get_polynomial_interpolation(self):
+        px = np.array(self.points_x)
+        py = np.array(self.points_y)
+        sorted_index = np.argsort(px)
 
-poly = np.vectorize(get_interpolation_polynomial(points))
+        px = px[sorted_index]
+        py = py[sorted_index]
+        def poly(x):
+            y = 0
+            for i in range(len(px)):
+                prod = py[i]
+                for j in range(len(px)):
+                    if j == i: continue
+                    prod *= (x - px[j]) / (px[i] - px[j])
+                y += prod
+            return np.clip(y, 0, 1)
+        
+        return np.vectorize(poly)
+    
+    def reset(self):
+        self.points_x = [0,1]
+        self.points_y = [0,1]
+        self.poly = self.get_polynomial_interpolation()
+        self.update_curve()
 
-root = tk.Tk()
-frame = tk.Frame(root)
+    def apply(self):
+        self.app.modified_image_data = self.poly(self.app.modified_image_data)
+        self.app.image_data = self.app.modified_image_data
+        self.app.update_canvas(self.app.modified_image_data)
+        self.root.destroy()
 
-fig = Figure()
-ax = fig.add_subplot(111)
-
-arr = np.array(points)
-ax.scatter(arr[:, 0], poly(arr[:, 0]), marker='x')
-
-graph = ax.plot(data, poly(data))
-canvas = FigureCanvasTkAgg(fig, master=root)
-
-canvas.draw()
-canvas.get_tk_widget().pack(side='top', fill='both', expand=1)
-frame.pack()
-
-root.mainloop()
+if '__name__' == 'main': 
+    CurveEditor()
